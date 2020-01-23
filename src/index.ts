d4ffa6faeea5faff5b4384df81626c9cedfa416d
Raphaël Thériault <raphael_theriault@outlook.com>
2020-01-23 15:14:13 -0500

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 import "reflect-metadata";
+import Koa from "koa";
+import Logger from "./logger";
+import bodyParser from "koa-bodyparser";
+import { createConnection } from "typeorm";
 
-console.log("Hello, World!");
+createConnection({
+    type: "postgres",
+    database: "scorebloq",
+    synchronize: true,
+    logging: true,
+    logger: new Logger(),
+})
+    .then(async () => {
+        const app = new Koa();
+        app.use(bodyParser());
+
+        app.listen(8080);
+    })
+    .catch(console.error);
---
+++ src/logger.ts
@@ -0,0 +1,84 @@
+import { Logger as L4JSLogger, configure, getLogger } from "log4js";
+import { Logger as TOLogger } from "typeorm";
+
+const logger = getLogger();
+logger.level = process.env.NODE_ENV === "production" ? "info" : "trace";
+configure({
+    appenders: { file: { type: "file", filename: "scorebloq" } },
+    categories: { default: { appenders: ["file"], level: logger.level } },
+});
+
+export default class Logger implements TOLogger {
+    private static logger: L4JSLogger = logger;
+
+    logQuery(query: string, parameters?: any[]): void {
+        logger.debug(
+            `SQL Query: ${query}${Logger.parametersToString(parameters)}`
+        );
+    }
+    logQueryError(error: string, query: string, parameters?: any[]): void {
+        logger.error(
+            `Failed SQL Query: ${query}${Logger.parametersToString(parameters)}`
+        );
+        logger.error(error);
+    }
+    logQuerySlow(time: number, query: string, parameters?: any[]): void {
+        logger.warn(
+            `Slow SQL Query (${time}): ${query}${Logger.parametersToString(
+                parameters
+            )}`
+        );
+    }
+    logSchemaBuild(message: string): void {
+        logger.info(message);
+    }
+    logMigration(message: string): void {
+        logger.info(message);
+    }
+    log(level: "log" | "info" | "warn", message: any): void {
+        switch (level) {
+            case "log":
+                logger.debug(message);
+                break;
+            case "info":
+                logger.info(message);
+                break;
+            case "warn":
+                logger.warn(message);
+                break;
+            default:
+                throw new TypeError("Invalid log level");
+        }
+    }
+
+    private static parametersToString(parameters?: any[]): string {
+        if (!parameters) {
+            return "";
+        }
+
+        try {
+            return ` -- PARAMETERS: ${JSON.stringify(parameters)}`;
+        } catch {
+            return ` -- PARAMETERS: ${parameters.toString()}`;
+        }
+    }
+
+    trace(message: string): void {
+        logger.trace(message);
+    }
+    debug(message: string): void {
+        logger.debug(message);
+    }
+    info(message: string): void {
+        logger.info(message);
+    }
+    warn(message: string): void {
+        logger.warn(message);
+    }
+    error(message: string): void {
+        logger.error(message);
+    }
+    fatal(message: string): void {
+        logger.fatal(message);
+    }
+}
